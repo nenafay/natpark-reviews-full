c7403e1434d87b9f20e5bedd85812139563ed781
nenafay <nena.park@gmail.com>
2019-07-13 13:08:40 -0400

    first commit

---
+++ natpark-reviews-full/src/main/java/nenafay/natparkreviewsfull/NatparkReviewsFullApplication.java
@@ -0,0 +1,13 @@
+package nenafay.natparkreviewsfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatparkReviewsFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatparkReviewsFullApplication.class, args);
+	}
+
+}
